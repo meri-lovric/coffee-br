028bbe57860d4e39190aed5aeed739652da192eb
merahxes <41114145+merahxes@users.noreply.github.com>
2019-01-11 15:59:14 +0100

    Add files via upload
    podjela u styles folder

--- main.js
+++ main.js
@@ -68,13 +68,6 @@ function addUser(evt, TabName){
 }
 
 /* COFFEE COUNTER */
-// onScreen jQuery plugin v0.2.1
-// (c) 2011-2013 Ben Pickles
-//
-// https://benpickles.github.io/onScreen
-//
-// Released under MIT license.
-(function(a){a.expr[":"].onScreen=function(b){var c=a(window),d=c.scrollTop(),e=c.height(),f=d+e,g=a(b),h=g.offset().top,i=g.height(),j=h+i;return h>=d&&h<f||j>d&&j<=f||i>e&&h<=d&&j>=f}})(jQuery);
 
 // Check if an element is on sceen every 2 seconds. Add and remove "visbile" class as applicable.
 setInterval(function() {
