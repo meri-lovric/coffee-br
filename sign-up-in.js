9ff87352754e20245106bee198273b7fc8b45b69
dsoljan <46396047+dsoljan@users.noreply.github.com>
2019-01-13 13:59:50 +0100

    Add files via upload
    nakon cca 24 sata zajebancije.
    u ovom momentu radi otvaranje prozora za registraciju/prijavu, ispunjavanje forme i zatvaranje bez da igdi proslijedi. iman jos dodat/popravit prompt za odjavu i opceniti css jer izgleda prelose.

---
+++ sign-up-in.js
@@ -0,0 +1,78 @@
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+    var $this = $(this),
+        label = $this.prev('label');
+  
+        if (e.type === 'keyup') {
+              if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+      } else if (e.type === 'blur') {
+          if( $this.val() === '' ) {
+              label.removeClass('active highlight'); 
+              } else {
+              label.removeClass('highlight');   
+              }   
+      } else if (e.type === 'focus') {
+        
+        if( $this.val() === '' ) {
+              label.removeClass('highlight'); 
+              } 
+        else if( $this.val() !== '' ) {
+              label.addClass('highlight');
+              }
+      }
+  
+  });
+  
+ //prebacivanje s login na reg 
+$('.tag a').on('click', function (e) {
+    
+    e.preventDefault();
+    
+    $(this).parent().addClass('active'); 
+    $(this).parent().siblings().removeClass('active');
+    
+    target = $(this).attr('href');
+  
+    $('.tab-content > div').not(target).hide();
+    
+    $(target).fadeIn(600);
+    
+  });
+
+  $('.form').submit(function () {
+    sendForm();
+    return false;
+   });
+
+  function sendForm (){
+    window.location.href = "file:///C:/Users/KORISNICA/Downloads/coffee-br-master%20(3)/coffee-br-master/index.html"
+  }
+
+document.getElementById('login-icon').addEventListener("click", function() {
+  document.querySelector('.bg-modal').style.display = "flex";
+  });
+  
+document.querySelector('.form .close').addEventListener("click", function() {
+  document.querySelector('.bg-modal').style.display = "none";
+  });
+
+document.getElementById('logout-icon').addEventListener("click", function() {
+  document.querySelector('.modal-box').style.display = "flex";
+  });
+  
+document.querySelector('.modal-box .close').addEventListener("click", function() {
+  document.querySelector('.modal-box').style.display = "none";
+  });
+
+document.querySelector('.button-block').addEventListener("click", function() {
+    document.querySelector('.bg-modal').style.display = "none";
+  });
+/*
+document.querySelector('#login .button-block').addEventListener("click", function() {
+    document.querySelector('.bg-modal').style.display = "none";
+  });*/
+
