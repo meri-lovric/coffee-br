2edc10af8bf758d1671c7b109de855f03a24b5e1
merahxes <41114145+merahxes@users.noreply.github.com>
2019-01-13 19:24:19 +0100

    Odabir stola u formi narudžbe
    Odabirom stola u padajućem izborniku forme narudžba korespondentni stol u kartici "Welcome" se zacrveni, i postane nedostupan u padajućem izborniku

---
+++ add-table.js
@@ -0,0 +1,14 @@
+let nextIcon = document.getElementById("btn");
+nextIcon.addEventListener("click", function (){
+    //console.log(e.target.options[e.target.selectedIndex].text);
+    var e = document.getElementById("table");
+    var table_value = e.options[e.selectedIndex].value;
+    var table = e.options[e.selectedIndex];
+    let tableIcons = document.querySelectorAll(".layout__seat");
+        for(let j=0;j<tableIcons.length;j++){
+            if(tableIcons[j].textContent == table_value){
+                    tableIcons[j].classList.add("layout__seat--selected");
+                    table.setAttribute('disabled', true);
+            }
+        }
+    });
